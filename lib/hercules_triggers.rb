8bda7edc70aea292521c7620392f20a4e84c17fb
Softa <contato@softa.com.br>
2010-11-12 15:17:32 -0400

    first commit

---
+++ lib/hercules_triggers.rb
@@ -0,0 +1,17 @@
+module Hercules
+  class Triggers
+    def self.before_deploy(options)
+      env = (options[:branch] == "master" ? "development" : options[:branch])
+      cmd = options[:shell]
+      cmd.run "cp config/pivotal_tracker.sample.yml config/pivotal_tracker.yml"
+      cmd.run "cp config/irc.sample.yml config/irc.yml"
+      true
+    end 
+
+    def self.after_deploy(options)
+      cmd = options[:shell]
+      cmd.run "kill -HUP \`cat /home/mailee/pids/#{options[:branch]}/site-unicorn.pid\`"
+    end 
+  end 
+end
+
